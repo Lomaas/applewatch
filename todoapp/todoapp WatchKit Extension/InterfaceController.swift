a6a80bdf03ba42a2dc5ae38fdce654dfa4ce27df
Simen Johannessen <simenlomaas@gmail.com>
2015-06-16 19:49:04 +0200

    remove code

--- todoapp/todoapp WatchKit Extension/InterfaceController.swift
+++ todoapp/todoapp WatchKit Extension/InterfaceController.swift
@@ -1,39 +1,9 @@
-//
-//  InterfaceController.swift
-//  todoapp WatchKit Extension
-//
-//  Created by Simen Johannessen on 15/06/15.
-//  Copyright © 2015 bekk. All rights reserved.
-//
-
 import WatchKit
 import Foundation
-import WatchCom
 
-
-class InterfaceController: WKInterfaceController {
-    static let rowTypeIdentifier = "mainRowType"
-    
-    var tableData = [
-        Todo(title: "Wash diches", date: NSDate()),
-        Todo(title: "Buy milk", date: NSDate()),
-        Todo(title: "Read doc.", date: NSDate())
-    ]
-    
-    @IBOutlet var tableView: WKInterfaceTable!
-    
+class InterfaceController: WKInterfaceController {    
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
-        WKInterfaceController.openParentApplication(["r" : "fsf"]) {
-            (replyInfo, error) -> Void in
-            
-        }
-        configureTable()
-    }
-    
-    override func table(table: WKInterfaceTable, didSelectRowAtIndex rowIndex: Int) {
-        tableData.removeAtIndex(rowIndex)
-        configureTable()
     }
 
     override func willActivate() {
@@ -42,16 +12,5 @@ class InterfaceController: WKInterfaceController {
 
     override func didDeactivate() {
         super.didDeactivate()
-    }
-
-    private func configureTable() {
-        tableView.setNumberOfRows(tableData.count, withRowType: InterfaceController.rowTypeIdentifier)
-        print("row data: \(tableView.numberOfRows)")
-        
-        for var index = 0; index < tableView.numberOfRows; index++ {
-            let row = tableView.rowControllerAtIndex(index) as! TodoRowController
-            let todo = tableData[index]
-            row.rowTitle.setText(todo.title)
-        }
     }
 }
